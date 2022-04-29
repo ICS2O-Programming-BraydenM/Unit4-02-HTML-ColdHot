9404fc3fcfd66570e89195e6ca25be10be4ad1a7
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-04-29 14:14:18 +0000

    fixed button

--- js/script.js
+++ js/script.js
@@ -9,19 +9,22 @@
 // This function displays a statement to see if the inputted temperature is considered hot or cold in my opinion 
  
 function onButtonClick () {
+
+  // declare constants 
+  const DEGREES_THRESHOLD = 15;
   
 	// get user input (degrees in celsius)
 	let temperature = parseInt(document.getElementById('degrees').value)
 
   // if statements, if number is correct, display a message that tells them so, if number is incorrect, display a message that tells them so 
-	if (temperature >= 15) {
+	if (temperature >= DEGREES_THRESHOLD) {
 		let greeting = "It is hot outside!"
-    document.getElementById('greeting').innerHTML = greeting
+    document.getElementById('display-results').innerHTML = greeting
 	}
 
   else {
-    let greeting = "it is cold outside."
-    document.getElementById('greeting').innerHTML = greeting + " The correct Number is " + correct + "."
+    let greeting = "It is cold outside."
+    document.getElementById('display-results').innerHTML = greeting 
   }
 }
 
